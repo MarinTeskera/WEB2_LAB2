10a00d51ccf044740b5e9a6b7efe08b8e73d2ca1
Marin Teskera <marin.teskera@infinum.com>
2023-11-12 11:57:50 +0100

    add login function

--- backend/src/helpers/auth.ts
+++ backend/src/helpers/auth.ts
@@ -7,5 +7,25 @@ export const register = async (username: string, password: string) => {
 
   const query = `INSERT INTO users (username, password) VALUES (${username}, ${password}) RETURNING *`;
 
-  return db.query(query);
+  try {
+    await db.query(query);
+    return username;
+  } catch {
+    throw new Error("username already exists");
+  }
+};
+
+export const login = async (username: string, password: string) => {
+  if (!username || !password) {
+    throw new Error("username and password required");
+  }
+
+  const query = `SELECT username FROM users WHERE username = ${username} AND password = ${password}`;
+
+  try {
+    const user = await db.query(query);
+    return user[0];
+  } catch {
+    throw new Error("username or password incorrect");
+  }
 };
