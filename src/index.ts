412a04673130ba4b65ad014cf5a02c2073bba132
Marin Teskera <marin.teskera@infinum.com>
2023-11-12 17:38:35 +0100

    create index view

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
-import session from "express-session";
-import authRoute from "./routes/auth.route";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import session from "express-session";
+import path from "path";
+import authRoute from "./routes/auth.route";
 
 const app = express();
-const port = process.env.PORT || 4200;
+const port = process.env.PORT || 3000;
 
 declare module "express-session" {
   export interface SessionData {
     user: string;
   }
 }
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -27,6 +32,10 @@ app.use(
 );
 
 app.use("/auth", authRoute);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
